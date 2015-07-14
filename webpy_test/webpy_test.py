4979c693df84cf7d234ca148f3b839556d1970da
ernienoternier <eerrddff@foxmail.com>
2015-07-14 05:55:30 -0700

    todos finished

--- webpy_test/webpy_test.py
+++ webpy_test/webpy_test.py
@@ -1,14 +1,26 @@
 import web
 
 urls = (
-'/(.*)', 'index'
+'/todos', 'todo',
+'/todos/add', 'todoadd'
 )
+
+db = web.database(dbn='mysql', user='root', pw='888888', db='webpy_test')
 
 render = web.template.render('templates/')
 
 class index:
 	def GET(self, name):
 		return render.index(name)
+class todo:
+	def GET(self):
+		todos = db.select('todo')
+		return render.todo(todos)
+class todoadd:
+	def POST(self):
+		i = web.input()
+		n = db.insert('todo', title=i.title)
+		raise web.seeother('/todos')
 
 
 if __name__ == "__main__":
